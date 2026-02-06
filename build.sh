c14b4540e7ebb99c513968e94d5bafe4cac44048
TheKit <thekit@disroot.org>
2026-02-06 18:30:03 +0200

    android-rootfs: include prebuilt android-rootfs.img for wseries
    As the device is not Treblized and there are no Halium 11 arm32
    generic builds, that's the easiest workaround for now.

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -xe
 [ -d build ] || git clone https://github.com/NotKit/halium-generic-adaptation-build-tools.git -b wseries build
+
+# Extract android-rootfs.img if missing or older than in the repo
+if [ ! -f overlay/system/var/lib/lxc/android/android-rootfs.img ] || [ android-rootfs/android-rootfs.img.zst.part00 -nt overlay/system/var/lib/lxc/android/android-rootfs.img ]; then
+    echo "Unpacking android-rootfs.img..."
+    mkdir -p overlay/system/var/lib/lxc/android
+    cat android-rootfs/android-rootfs.img.zst.part* | zstd -d > overlay/system/var/lib/lxc/android/android-rootfs.img
+fi
+
 ./build/build.sh "$@"
